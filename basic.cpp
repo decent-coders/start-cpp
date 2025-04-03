242509a13960955f3bb0c8636e5acdecd212c493
Hridoy Hawladar <decentcoderz@gmail.com>
2025-04-03 23:17:43 +0800

    fasdf

--- basic.cpp
+++ basic.cpp
@@ -329,3 +329,21 @@
 //     cout<<"Factorial of "<<x<<" is "<<factorial(x);
 //     return 0;
 // }
+// #include <iostream>
+// using namespace std;
+// int factorial(int n){
+//     int fact = 1;
+//     for (int i = 1; i <= n; i++)
+//     {
+//        fact *= i;
+//     }
+
+//     return fact;
+// }
+// int main(){
+//     cout<<"Enter a number: ";
+//     int x;
+//     cin>>x;
+//     cout<<"Factorial of "<<x<<" is "<<factorial(x);
+//     return 0;
+// }
