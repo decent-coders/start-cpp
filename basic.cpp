68b50ec001b2a1f099b40b1f8fa83e42d89e8bc6
Hridoy Hawladar <decentcoderz@gmail.com>
2025-04-09 23:43:32 +0800

    afadfadfs

--- basic.cpp
+++ basic.cpp
@@ -753,24 +753,45 @@
 //     }
 //     return 0;
 // }
-#include <iostream>
-using namespace std;
-int linearSearch( int arr[],int size, int target)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == target)
-        {
-            return i;
-        }
-    }
-    return -1; // Target not found
-};
+// #include <iostream>
+// using namespace std;
+// int linearSearch( int arr[],int size, int target)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (arr[i] == target)
+//         {
+//             return i;
+//         }
+//     }
+//     return -1; // Target not found
+// };
 
-int main()
-{ 
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
-    int size = 10;
-    int target = 5;
-    cout << linearSearch(arr, size, target) << endl;
-}
+// int main()
+// { 
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//     int size = 10;
+//     int target = 5;
+// //     cout << linearSearch(arr, size, target) << endl;
+// // }
+// #include <iostream>
+// using namespace std;
+// int linearSearch(int arr[], int size, int target)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (arr[i] == target)
+//         {
+//             return i;
+//         }
+//     }
+//     return -1; // Target not found
+// };
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int size = 10;
+//     int target = 5;
+//     cout << linearSearch(arr, size, target) << endl;
+// }
