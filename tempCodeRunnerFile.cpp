6a79cea0f8d0a4c0f287a9bf45b59b1b03cd7b7c
Hridoy Hawladar <decentcoderz@gmail.com>
2025-04-26 20:46:58 +0800

    array

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,21 @@
-        // if (test[i] < smallest)
-        // {
-        //     smallest = test[i];
-        // }
+#include <iostream>
+using namespace std;
+int linearSearch(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1; // Target not found
+};
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+    int target = 5;
+    cout << linearSearch(arr, size, target) << endl;
+}
